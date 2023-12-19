bf1d95fc83088208f51e4941452863af7a4c1f52
Wendell Turner <wturner@cssiinc.com>
2023-12-19 14:57:38 -0500

    first version

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -67,6 +67,39 @@ return {
         },
       },
     }
+
+  -- ++++++++++++++++++
+  -- mostly from:  https://jdhao.github.io/2023/07/22/neovim-pylsp-setup/
+
+  lspconfig.pylsp.setup {
+    on_attach = custom_attach,
+    settings = {
+      pylsp = {
+        plugins = {
+          -- formatter options
+          --wt: black = { enabled = true },
+          autopep8 = { enabled = false },
+          yapf = { enabled = false },
+          -- linter options
+          pylint = { enabled = true, executable = "pylint" },
+          pyflakes = { enabled = false },
+          pycodestyle = { enabled = false },
+          -- type checker
+          pylsp_mypy = { enabled = true },
+          -- auto-completion options
+          jedi_completion = { fuzzy = true },
+          -- import sorting
+          pyls_isort = { enabled = true },
+        },
+      },
+    },
+    flags = {
+        debounce_text_changes = 200,
+    },
+    capabilities = capabilities,
+  }
+  -- ++++++++++++++++++
+
   end
 }
 
