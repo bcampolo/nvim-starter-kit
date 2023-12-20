d1af22c819995c342b1a08cdc2f2a9c331e1d8a6
Wendell Turner <wturner@cssiinc.com>
2023-12-19 21:33:48 -0500

    fix undefined; indentation

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -68,37 +68,37 @@ return {
       },
     }
 
-  -- ++++++++++++++++++
-  -- mostly from:  https://jdhao.github.io/2023/07/22/neovim-pylsp-setup/
+    -- ++++++++++++++++++
+    -- mostly from:  https://jdhao.github.io/2023/07/22/neovim-pylsp-setup/
 
-  lspconfig.pylsp.setup {
-    on_attach = custom_attach,
-    settings = {
-      pylsp = {
-        plugins = {
-          -- formatter options
-          --no thanks: black = { enabled = true },
-          autopep8 = { enabled = false },
-          yapf = { enabled = false },
-          -- linter options
-          pylint = { enabled = true, executable = "pylint" },
-          pyflakes = { enabled = false },
-          pycodestyle = { enabled = false },
-          -- type checker
-          pylsp_mypy = { enabled = true },
-          -- auto-completion options
-          jedi_completion = { fuzzy = true },
-          -- import sorting
-          pyls_isort = { enabled = true },
+    -- pylint settings
+    lspconfig.pylsp.setup {
+      on_attach = lsp_attach,
+      settings = {
+        pylsp = {
+          plugins = {
+            -- formatter options
+            --no thanks: black = { enabled = true },
+            autopep8 = { enabled = false },
+            yapf = { enabled = false },
+            -- linter options
+            pylint = { enabled = true, executable = "pylint" },
+            pyflakes = { enabled = false },
+            pycodestyle = { enabled = false },
+            -- type checker
+            pylsp_mypy = { enabled = true },
+            -- auto-completion options
+            jedi_completion = { fuzzy = true },
+            -- import sorting
+            pyls_isort = { enabled = true },
+          },
         },
       },
-    },
-    flags = {
-        debounce_text_changes = 200,
-    },
-    capabilities = capabilities,
-  }
-  -- ++++++++++++++++++
+      flags = {
+          debounce_text_changes = 200,
+      },
+      capabilities = lsp_capabilities,
+    }
 
   end
 }
