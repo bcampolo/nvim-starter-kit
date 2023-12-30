b17d2d4bae22610c29360619589247879910c16f
bcampolo <bjcampolo@gmail.com>
2023-12-30 09:37:58 -0500

    Update comment and indentation for contribution from @sayanta01

--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -2,10 +2,11 @@
 return {
   -- https://github.com/christoomey/vim-tmux-navigator
   'christoomey/vim-tmux-navigator',
-  		event = function()
-			if vim.fn.exists("$TMUX") == 1 then
-				return "VeryLazy"
-			end
-		end,
+  -- Only load this plugin if tmux is being used
+  event = function()
+    if vim.fn.exists("$TMUX") == 1 then
+      return "VeryLazy"
+    end
+  end,
 }
 
