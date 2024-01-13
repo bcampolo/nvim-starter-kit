13825f219a5cbb718dc5591f4905c26612f4dd2e
bcampolo <bjcampolo@gmail.com>
2024-01-12 20:28:50 -0500

    Update nvim-cmp keymaps to support tabbing through snippets properly

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -39,17 +39,37 @@ return {
       completion = {
         completeopt = 'menu,menuone,noinsert',
       },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-        ["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4), -- scroll backward
-        ["<C-f>"] = cmp.mapping.scroll_docs(4), -- scroll forward
-        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(), -- clear completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- confirm selection
-      }),
+      mapping = cmp.mapping.preset.insert {
+        ['<C-j>'] = cmp.mapping.select_next_item(), -- next suggestion
+        ['<C-k>'] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4), -- scroll backward
+        ['<C-f>'] = cmp.mapping.scroll_docs(4), -- scroll forward
+        ['<C-Space>'] = cmp.mapping.complete {}, -- show completion suggestions
+        ['<CR>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
+	-- Tab through suggestions or when a snippet is active, tab to the next argument
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+	-- Tab backwards through suggestions or when a snippet is active, tab to the next argument
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      },
       sources = cmp.config.sources({
         { name = "nvim_lsp" }, -- lsp 
         { name = "luasnip" }, -- snippets
